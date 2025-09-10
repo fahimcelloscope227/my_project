a807cefaeb5392a95aa4badc01d86a90a23e2668
fahimcelloscope227 <fahim.celloscope@gmail.com>
2025-09-10 11:45:20 +0600

    test: add summation tests

---
+++ test/flutter_test/calculator_test.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_project/main.dart' as app; // change my_app to your actual package name
+
+
+void main(){
+
+  group("Calculator Testing", ()  {
+    testWidgets("adds two numbers", (tester)async{
+      // start app
+      await tester.pumpWidget(app.MyApp());
+
+      // find the text fields
+      final textFieldA = find.byType(TextFormField).at(0);
+      final textFieldB = find.byType(TextFormField).at(1);
+      final addButton = find.byType(ElevatedButton).at(0);
+
+      // enter numbers
+      await tester.enterText(textFieldA,'5');
+      await tester.enterText(textFieldB,'2');
+
+      await tester.tap(addButton);
+      await tester.pump();
+
+      expect(find.text('7'), findsOneWidget);
+    });
+  });
+}
