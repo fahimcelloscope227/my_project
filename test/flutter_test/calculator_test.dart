04ed81609613af5f5e0897922d3f4c6d686d7fad
fahimcelloscope227 <fahim.celloscope@gmail.com>
2025-09-10 11:54:09 +0600

    test: add test case for substraction

--- test/flutter_test/calculator_test.dart
+++ test/flutter_test/calculator_test.dart
@@ -25,5 +25,24 @@ void main(){
 
       expect(find.text('7'), findsOneWidget);
     });
+
+    testWidgets("substract two numbers", (tester)async{
+      // start app
+      await tester.pumpWidget(app.MyApp());
+
+      // find the text fields
+      final textFieldA = find.byType(TextFormField).at(0);
+      final textFieldB = find.byType(TextFormField).at(1);
+      final substractButton = find.byType(ElevatedButton).at(1);
+
+      // enter numbers
+      await tester.enterText(textFieldA,'5');
+      await tester.enterText(textFieldB,'2');
+
+      await tester.tap(substractButton);
+      await tester.pump();
+
+      expect(find.text('3'), findsOneWidget);
+    });
   });
 }
