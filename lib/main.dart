f94fc4e824507edcbd29e94973829a8bfb4f6f5e
fahimcelloscope227 <fahim.celloscope@gmail.com>
2025-09-15 14:38:44 +0600

    feat: fingerprint auth with local_auth feat: freezed using model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/features/biometric_auth/presentation/biometric_auth_page.dart';
+import 'package:my_project/features/loca_auth/presenter/finger_print_auth.dart' show FingerPrintAuth;
 
 void main() {
   runApp(const MyApp());
@@ -17,10 +18,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
 
       ),
-      initialRoute: "/bio_auth",
+      initialRoute: "/local_auth",
       routes: {
         "/": (context) => MyHomePage(title: '',),
         "/bio_auth": (context) =>BiometricAuthPage(),
+        "/local_auth" : (context) => FingerPrintAuth()
       },
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
